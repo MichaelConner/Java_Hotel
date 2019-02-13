4cdb20184410aa22d0fb5ef8fe7431c1ab8f376a
Michael Conner <michaelconner@hotmail.co.uk>
2019-02-13 11:30:47 +0000

    hotel class created

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,57 @@
+import java.util.ArrayList;
+
 public class Hotel {
+
+
+    private String name;
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<DiningRoom> diningRooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+
+
+    public Hotel(String name){
+        this.name = name;
+        this.bedrooms = new ArrayList<>();
+        this.diningRooms = new ArrayList<>();
+        this.conferenceRooms = new ArrayList<>();
+    }
+
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getBedroomCount(){
+        return this.bedrooms.size();
+    }
+
+    public int getDiningRoomCount(){
+        return this.diningRooms.size();
+    }
+
+    public int getConferenceRoomCount(){
+        return this.conferenceRooms.size();
+    }
+
+    public void addBedroom(Bedroom bedroom){
+        bedrooms.add(bedroom);
+    }
+
+    public void addDiningRoom(DiningRoom diningRoom){
+        diningRooms.add(diningRoom);
+    }
+
+    public void addConferenceRoom(ConferenceRoom conferenceRoom){
+        conferenceRooms.add(conferenceRoom);
+    }
+
+    public int getTotalRoomCount(){
+        return(getBedroomCount() + getDiningRoomCount() + getConferenceRoomCount());
+    }
+
+
+
+
+
+
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,79 @@
+import com.sun.xml.internal.bind.v2.TODO;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
 public class HotelTest {
+
+    Hotel hotel;
+    Bedroom bedroom;
+    DiningRoom diningRoom;
+    ConferenceRoom conferenceRoom;
+
+
+    @Before
+    public void before(){
+
+        hotel = new Hotel("Step Back Inn");
+
+        bedroom = new Bedroom(1, 2, "Double");
+
+        diningRoom = new DiningRoom("The McCrannoch Lounge", 10);
+
+        conferenceRoom = new ConferenceRoom("The Stiletto Conference Room", 20);
+
+    }
+
+
+    @Test
+    public void hotelHasName(){
+        assertEquals("Step Back Inn", hotel.getName());
+    }
+
+    @Test
+    public void hasEmptyBedroomList(){
+        assertEquals(0, hotel.getBedroomCount());
+    }
+
+    @Test
+    public void hasEmptyDiningRoomList(){
+        assertEquals(0, hotel.getDiningRoomCount());
+    }
+
+    @Test
+    public void hasEmptyConferenceRoomList(){
+        assertEquals(0, hotel.getConferenceRoomCount());
+    }
+
+    @Test
+    public void canAddBedroom(){
+        hotel.addBedroom(bedroom);
+        assertEquals(1, hotel.getBedroomCount());
+    }
+
+    @Test
+    public void canAddDiningRoom(){
+        hotel.addDiningRoom(diningRoom);
+        assertEquals(1, hotel.getDiningRoomCount());
+    }
+
+    @Test
+    public void canAddConferenceRoom(){
+        hotel.addConferenceRoom(conferenceRoom);
+        assertEquals(1, hotel.getConferenceRoomCount());
+    }
+
+    @Test
+    public void canGetTotalRoomCount(){
+        hotel.addBedroom(bedroom);
+        hotel.addBedroom(bedroom);
+        hotel.addBedroom(bedroom);
+        hotel.addDiningRoom(diningRoom);
+        hotel.addDiningRoom(diningRoom);
+        hotel.addConferenceRoom(conferenceRoom);
+        assertEquals(6, hotel.getTotalRoomCount());
+    }
+
+
 }
